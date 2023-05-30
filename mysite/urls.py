fb22a1f5386906c77660cacc185766ef42cdedc9
Krishna Prabhakar <krishnaprabhakar@Krishnas-Air.fios-router.home>
2023-05-30 13:51:39 -0400

    application app1 created

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,13 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("This is main page")
+from django.urls import path, include
+from app1.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",index,name='index')
+    path("",index,name='index'),
 ]

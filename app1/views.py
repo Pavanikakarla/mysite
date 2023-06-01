2362534ddc228da5f196bd4e672b9ba6078c30cd
Krishna Prabhakar <krishnaprabhakar@Krishnas-MacBook-Air.local>
2023-06-01 12:45:09 -0400

    Views.py file modified

--- app1/views.py
+++ app1/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("This is main page")
+    return HttpResponse("<h1>This is main page</h1>")
